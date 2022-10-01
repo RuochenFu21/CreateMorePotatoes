89d15f52a763493ccff9da97ea4110dfe235a9ce
A_pythoner <62882800+Ruochenfu2011@users.noreply.github.com>
2022-10-01 22:47:05 +0800

    test: trying to make only tnt_potato can explode

--- src/main/java/net/forsteri/createmorepotatoes/mixin/EffectHandler.java
+++ src/main/java/net/forsteri/createmorepotatoes/mixin/EffectHandler.java
@@ -2,6 +2,7 @@ package net.forsteri.createmorepotatoes.mixin;
 
 import com.mojang.logging.LogUtils;
 import com.simibubi.create.content.curiosities.weapons.PotatoProjectileEntity;
+import net.forsteri.createmorepotatoes.item.ModItems;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.projectile.AbstractHurtingProjectile;
 import net.minecraft.world.item.ItemStack;
@@ -10,11 +11,13 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.EntityHitResult;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.registries.ForgeRegistries;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import net.forsteri.createmorepotatoes.item.ModItems;
 
 @Mixin(PotatoProjectileEntity.class)
 public abstract class EffectHandler extends AbstractHurtingProjectile {
@@ -29,11 +32,12 @@ public abstract class EffectHandler extends AbstractHurtingProjectile {
     protected void onHit(EntityHitResult ray, CallbackInfo info) {
         LogUtils.getLogger().info(getX() + " " + getY());
 
-        Explosion derp = new Explosion(getLevel(), this, getX(), getY(), getZ(), 3, false, Explosion.BlockInteraction.BREAK);
-        if (!level.isClientSide()) {
-            derp.explode();
+        if (stack.getItem() == ModItems.TNT_POTATO_ITEM) {
+            Explosion derp = new Explosion(getLevel(), this, getX(), getY(), getZ(), 3, false, Explosion.BlockInteraction.BREAK);
+            if (!level.isClientSide()) {
+                derp.explode();
+            }
+            derp.finalizeExplosion(true);
         }
-        derp.finalizeExplosion(true);
-
     }
 }
