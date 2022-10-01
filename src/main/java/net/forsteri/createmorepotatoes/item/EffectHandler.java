187a677e4fa0744561e61a0dc8ef07f84c4ea24e
A_pythoner <62882800+Ruochenfu2011@users.noreply.github.com>
2022-10-01 15:51:31 +0800

    test: testing Mixin

---
+++ src/main/java/net/forsteri/createmorepotatoes/item/EffectHandler.java
@@ -0,0 +1,18 @@
+package net.forsteri.createmorepotatoes.item;
+
+import com.mojang.logging.LogUtils;
+import com.simibubi.create.content.curiosities.weapons.PotatoProjectileEntity;
+import net.minecraft.world.phys.EntityHitResult;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(PotatoProjectileEntity.class)
+public class EffectHandler {
+    @Inject(at = @At(value = "HEAD"), method = "Lcom/simibubi/create/content/curiosities/weapons/PotatoProjectileEntity;onHitEntity(L;)V", cancellable = true)
+    private void onHit(EntityHitResult ray, CallbackInfo info) {
+
+        LogUtils.getLogger().debug("HI, Im random");
+    }
+}
