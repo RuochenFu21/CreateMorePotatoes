e80c4769ce1803a4b2756b217e63f5726661ed35
A_pythoner <62882800+Ruochenfu2011@users.noreply.github.com>
2022-10-01 23:18:44 +0800

    test: testing toString

--- src/main/java/net/forsteri/createmorepotatoes/mixin/EffectHandler.java
+++ src/main/java/net/forsteri/createmorepotatoes/mixin/EffectHandler.java
@@ -3,6 +3,7 @@ package net.forsteri.createmorepotatoes.mixin;
 import com.mojang.logging.LogUtils;
 import com.simibubi.create.content.curiosities.weapons.PotatoProjectileEntity;
 import net.forsteri.createmorepotatoes.item.ModItems;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.projectile.AbstractHurtingProjectile;
 import net.minecraft.world.item.ItemStack;
@@ -19,10 +20,14 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import net.forsteri.createmorepotatoes.item.ModItems;
 
+import java.util.Objects;
+
 @Mixin(PotatoProjectileEntity.class)
 public abstract class EffectHandler extends AbstractHurtingProjectile {
 
     @Shadow protected ItemStack stack;
+
+    @Shadow protected Entity stuckEntity;
 
     protected EffectHandler(EntityType<? extends AbstractHurtingProjectile> p_36833_, Level p_36834_) {
         super(p_36833_, p_36834_);
@@ -30,7 +35,7 @@ public abstract class EffectHandler extends AbstractHurtingProjectile {
 
     @Inject(at = @At(value = "HEAD"), method = "onHitEntity(Lnet/minecraft/world/phys/EntityHitResult;)V")
     protected void onHit(EntityHitResult ray, CallbackInfo info) {
-        LogUtils.getLogger().info(getX() + " " + getY());
+        LogUtils.getLogger().info(Objects.requireNonNull(stack.getItem().getRegistryName()).toString());
 
         if (true) {
             Explosion derp = new Explosion(getLevel(), this, getX(), getY(), getZ(), 3, false, Explosion.BlockInteraction.BREAK);
