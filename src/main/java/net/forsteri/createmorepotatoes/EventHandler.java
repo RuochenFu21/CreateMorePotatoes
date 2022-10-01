e61f53a6a8f2f9bfcfc27d6cf44445ac4ad429ea
A_pythoner <62882800+Ruochenfu2011@users.noreply.github.com>
2022-10-01 12:47:12 +0800

    test: EventHandler

--- src/main/java/net/forsteri/createmorepotatoes/EventHandler.java
+++ src/main/java/net/forsteri/createmorepotatoes/EventHandler.java
@@ -1,9 +1,12 @@
 package net.forsteri.createmorepotatoes;
 
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.entity.player.ArrowNockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import com.simibubi.create.events.ClientEvents;
+import net.minecraftforge.fml.common.Mod;
 
+@Mod.EventBusSubscriber(Dist.CLIENT)
 public class EventHandler {
     @SubscribeEvent
     public static void arrowNocked(ArrowNockEvent event) {
