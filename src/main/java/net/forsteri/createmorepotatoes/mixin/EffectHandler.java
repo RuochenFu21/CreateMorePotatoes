adce5b3b4c6688e04df106d03aa91e90c9c23ec8
A_pythoner <62882800+Ruochenfu2011@users.noreply.github.com>
2022-10-01 17:30:02 +0800

    test: show x and y of the projectile in log when hit

--- src/main/java/net/forsteri/createmorepotatoes/mixin/EffectHandler.java
+++ src/main/java/net/forsteri/createmorepotatoes/mixin/EffectHandler.java
@@ -2,17 +2,25 @@ package net.forsteri.createmorepotatoes.mixin;
 
 import com.mojang.logging.LogUtils;
 import com.simibubi.create.content.curiosities.weapons.PotatoProjectileEntity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.projectile.AbstractHurtingProjectile;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.EntityHitResult;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PotatoProjectileEntity.class)
-public class EffectHandler {
+public abstract class EffectHandler extends AbstractHurtingProjectile {
+
+    protected EffectHandler(EntityType<? extends AbstractHurtingProjectile> p_36833_, Level p_36834_) {
+        super(p_36833_, p_36834_);
+    }
+
     @Inject(at = @At(value = "HEAD"), method = "onHitEntity(Lnet/minecraft/world/phys/EntityHitResult;)V", cancellable = true)
     protected void onHit(EntityHitResult ray, CallbackInfo info) {
-
-        LogUtils.getLogger().info("HI, Im random");
+        LogUtils.getLogger().info(getX() + " " + getY());
     }
 }
