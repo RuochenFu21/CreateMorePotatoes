310436f42a427a5c5f784446a7bc13f8e5240fdd
A_pythoner <62882800+Ruochenfu2011@users.noreply.github.com>
2022-10-01 22:47:05 +0800

    test: trying to make only tnt_potato can explode

--- src/main/java/net/forsteri/createmorepotatoes/item/ModItems.java
+++ src/main/java/net/forsteri/createmorepotatoes/item/ModItems.java
@@ -3,22 +3,19 @@ package net.forsteri.createmorepotatoes.item;
 import net.forsteri.createmorepotatoes.CreateMorePotatoes;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
-import net.minecraftforge.event.entity.player.ArrowNockEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
-import com.simibubi.create.content.curiosities.weapons.PotatoProjectileEntity;
-import com.simibubi.create.content.curiosities.weapons.PotatoCannonProjectileType;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, CreateMorePotatoes.MOD_ID);
 
-    public static final RegistryObject<Item> TNT_POTATO = ITEMS.register("tnt_potato",
-            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.MORE_POTATOES_TAB)));
+    public static final Item TNT_POTATO_ITEM = new Item(new Item.Properties().tab(ModCreativeModeTab.MORE_POTATOES_TAB));
 
+    public static final RegistryObject<Item> TNT_POTATO = ITEMS.register("tnt_potato",
+            () -> TNT_POTATO_ITEM);
 
 
     public static void register(IEventBus eventBus){
