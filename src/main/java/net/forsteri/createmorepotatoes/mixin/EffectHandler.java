59fc3ef0c4441d1157ea3e55625e8cfc6d24e4e3
A_pythoner <62882800+Ruochenfu2011@users.noreply.github.com>
2022-10-01 21:50:18 +0800

    fix: small fix of chance of no explosion particle and sound in client

--- src/main/java/net/forsteri/createmorepotatoes/mixin/EffectHandler.java
+++ src/main/java/net/forsteri/createmorepotatoes/mixin/EffectHandler.java
@@ -4,18 +4,22 @@ import com.mojang.logging.LogUtils;
 import com.simibubi.create.content.curiosities.weapons.PotatoProjectileEntity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.projectile.AbstractHurtingProjectile;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Explosion;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.EntityHitResult;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PotatoProjectileEntity.class)
 public abstract class EffectHandler extends AbstractHurtingProjectile {
+
+    @Shadow protected ItemStack stack;
 
     protected EffectHandler(EntityType<? extends AbstractHurtingProjectile> p_36833_, Level p_36834_) {
         super(p_36833_, p_36834_);
@@ -28,9 +32,8 @@ public abstract class EffectHandler extends AbstractHurtingProjectile {
         Explosion derp = new Explosion(getLevel(), this, getX(), getY(), getZ(), 3, false, Explosion.BlockInteraction.BREAK);
         if (!level.isClientSide()) {
             derp.explode();
-            derp.finalizeExplosion(true);
-        }else {
-            derp.finalizeExplosion(true);
         }
+        derp.finalizeExplosion(true);
+
     }
 }
