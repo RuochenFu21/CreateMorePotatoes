ed6023ed9e99dc9bf38e5a4eff66f8bfc46d5a52
A_pythoner <62882800+Ruochenfu2011@users.noreply.github.com>
2022-10-03 17:36:04 +0800

    feat(test): golden_potato

--- src/main/java/net/forsteri/createmorepotatoes/item/ModFoods.java
+++ src/main/java/net/forsteri/createmorepotatoes/item/ModFoods.java
@@ -6,4 +6,6 @@ import net.minecraft.world.food.FoodProperties;
 
 public class ModFoods {
     public static final FoodProperties EXPLOSIVE_POTATO = (new FoodProperties.Builder()).nutrition(1).saturationMod(0.3F).effect(new MobEffectInstance(MobEffects.CONFUSION, 300, 1), 1.0F).effect(new MobEffectInstance(MobEffects.POISON, 100, 0), 0.3F).alwaysEat().build();
+
+    public static final FoodProperties GOLDEN_POTATO = (new FoodProperties.Builder()).nutrition(4).saturationMod(1.2F).effect(new MobEffectInstance(MobEffects.REGENERATION, 100, 1), 1.0F).effect(new MobEffectInstance(MobEffects.ABSORPTION, 2400, 0), 1.0F).alwaysEat().build();
 }
--- src/main/java/net/forsteri/createmorepotatoes/item/ModItems.java
+++ src/main/java/net/forsteri/createmorepotatoes/item/ModItems.java
@@ -15,6 +15,10 @@ public class ModItems {
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.MORE_POTATOES_TAB).food(ModFoods.EXPLOSIVE_POTATO)));
 
 
+    public static final RegistryObject<Item> GOLDEN_POTATO = ITEMS.register("golden_potato",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.MORE_POTATOES_TAB).food(ModFoods.GOLDEN_POTATO)));
+
+
     public static final RegistryObject<Item> BAG_OF_POTATOES = ITEMS.register("bag_of_potatoes",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.MORE_POTATOES_TAB)));
 
