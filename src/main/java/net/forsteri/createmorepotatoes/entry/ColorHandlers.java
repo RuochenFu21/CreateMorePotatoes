dc73909e86bf12a594d1f55522e587b7b46cf388
A_pythoner <62882800+Ruochenfu2011@users.noreply.github.com>
2022-10-15 17:47:35 +0800

    test: try adding color to potion potatoes

--- src/main/java/net/forsteri/createmorepotatoes/entry/ColorHandlers.java
+++ src/main/java/net/forsteri/createmorepotatoes/entry/ColorHandlers.java
@@ -3,6 +3,7 @@ package net.forsteri.createmorepotatoes.entry;
 import com.mojang.logging.LogUtils;
 import net.forsteri.createmorepotatoes.CreateMorePotatoes;
 import net.forsteri.createmorepotatoes.item.PotionPotatoItem;
+import net.minecraft.world.item.alchemy.PotionUtils;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.ColorHandlerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -14,6 +15,6 @@ public class ColorHandlers {
     public static void registerItemColors(ColorHandlerEvent.Item event)
     {
         LogUtils.getLogger().info("REGISTER ITEM COLORS");
-        event.getItemColors().register(((pStack, pTintIndex) -> (0x555555)), ModItems.POTION_POTATO.get());
+        event.getItemColors().register(((pStack, pTintIndex) -> (PotionUtils.getColor(pStack))), ModItems.POTION_POTATO.get());
     }
 }
