f36cdc44d166c3a86bca9f7f57ba26aa4899c18c
A_pythoner <62882800+Ruochenfu2011@users.noreply.github.com>
2022-10-15 22:45:53 +0800

    test: hit effect

--- src/main/java/net/forsteri/createmorepotatoes/mixin/EffectHandler.java
+++ src/main/java/net/forsteri/createmorepotatoes/mixin/EffectHandler.java
@@ -3,12 +3,15 @@ package net.forsteri.createmorepotatoes.mixin;
 import com.mojang.logging.LogUtils;
 import com.simibubi.create.content.curiosities.weapons.PotatoProjectileEntity;
 import net.forsteri.createmorepotatoes.entry.ModItems;
+import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.projectile.AbstractHurtingProjectile;
 import net.minecraft.world.item.ItemStack;
 
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.alchemy.PotionUtils;
 import net.minecraft.world.level.Explosion;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.BlockHitResult;
@@ -43,6 +46,12 @@ public abstract class EffectHandler extends AbstractHurtingProjectile {
                 derp.explode();
             }
             derp.finalizeExplosion(true);
+        }
+
+        if (stack.is(ModItems.POTION_POTATO.get())) {
+            for (MobEffectInstance Effect : PotionUtils.getMobEffects(stack)) {
+                ((LivingEntity) ray.getEntity()).addEffect(Effect);
+            }
         }
     }
 
