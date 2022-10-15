c69a7885f19d79575b86d5690fcb4c8f5b9300a1
A_pythoner <62882800+Ruochenfu2011@users.noreply.github.com>
2022-10-15 10:41:45 +0800

    feat(test): new creative mode tab

--- src/main/java/net/forsteri/createmorepotatoes/item/PotionPotatoItem.java
+++ src/main/java/net/forsteri/createmorepotatoes/item/PotionPotatoItem.java
@@ -1,14 +1,8 @@
 package net.forsteri.createmorepotatoes.item;
 
-import net.forsteri.createmorepotatoes.entry.ModCreativeModeTab;
-import net.minecraft.client.color.item.ItemColors;
-import net.minecraft.world.effect.MobEffect;
-import net.minecraft.world.effect.MobEffectCategory;
-import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.PotionItem;
-import net.minecraft.world.item.alchemy.Potion;
 import net.minecraft.world.item.alchemy.PotionUtils;
 
 import java.util.Collection;
@@ -18,7 +12,7 @@ public class PotionPotatoItem extends PotionItem {
     private static final String NBT_DURATION = "Duration";
     private static final String NBT_AMPLIFIER = "Amplifier";
     public PotionPotatoItem(){
-        super(new Item.Properties().tab(ModCreativeModeTab.MORE_POTATOES_TAB));
+        super(new Item.Properties().tab(PotionPotatoCreativeModeTab.POTION_POTATOES_TAB));
     }
 
     public static int getPotionColor(ItemStack stack){
