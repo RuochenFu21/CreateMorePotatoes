b1982f2e122cd24248002e71aa399d51a0fe8d79
A_pythoner <62882800+Ruochenfu2011@users.noreply.github.com>
2022-10-17 21:29:42 +0800

    test: debug getRotationAxis and hasShaftTowards

--- src/main/java/net/forsteri/createmorepotatoes/tileEntity/stationaryPotatoCanon/StationaryPotatoCanonBlock.java
+++ src/main/java/net/forsteri/createmorepotatoes/tileEntity/stationaryPotatoCanon/StationaryPotatoCanonBlock.java
@@ -3,6 +3,7 @@ package net.forsteri.createmorepotatoes.tileEntity.stationaryPotatoCanon;
 import com.simibubi.create.content.contraptions.base.DirectionalKineticBlock;
 import com.simibubi.create.content.contraptions.base.KineticBlock;
 import com.simibubi.create.content.contraptions.base.RotatedPillarKineticBlock;
+import net.forsteri.createmorepotatoes.CreateMorePotatoes;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.LevelReader;
@@ -17,11 +18,13 @@ public class StationaryPotatoCanonBlock extends RotatedPillarKineticBlock {
 
     @Override
     public Direction.Axis getRotationAxis(BlockState state) {
+        CreateMorePotatoes.LOGGER.info(state.getValue(AXIS).toString());
         return state.getValue(AXIS);
     }
 
     @Override
     public boolean hasShaftTowards(LevelReader world, BlockPos pos, BlockState state, Direction face) {
+        CreateMorePotatoes.LOGGER.info("testing if has shaft");
         return face.getAxis() == getRotationAxis(state);
     }
 }
