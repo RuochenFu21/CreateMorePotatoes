bd6df96ff64197dcc7d216be655690f5311494d5
A_pythoner <62882800+Ruochenfu2011@users.noreply.github.com>
2022-10-17 15:58:06 +0800

    refactor(potion potatoes): removed unused variables

--- src/main/java/net/forsteri/createmorepotatoes/item/PotionPotatoItem.java
+++ src/main/java/net/forsteri/createmorepotatoes/item/PotionPotatoItem.java
@@ -8,9 +8,6 @@ import net.minecraft.world.item.alchemy.PotionUtils;
 import java.util.Collection;
 
 public class PotionPotatoItem extends PotionItem {
-    private static final String NBT_ID = "ID";
-    private static final String NBT_DURATION = "Duration";
-    private static final String NBT_AMPLIFIER = "Amplifier";
     public PotionPotatoItem(){
         super(new Item.Properties().tab(PotionPotatoCreativeModeTab.POTION_POTATOES_TAB));
     }
