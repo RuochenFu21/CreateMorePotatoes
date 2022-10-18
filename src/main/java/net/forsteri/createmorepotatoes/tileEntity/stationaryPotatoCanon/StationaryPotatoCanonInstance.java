69c611716ff0532ce37e5278f1477baff1cbdd34
A_pythoner <62882800+Ruochenfu2011@users.noreply.github.com>
2022-10-18 20:20:50 +0800

    refactor: StationaryPotatoCanonInstance now extends ShaftInstance

--- src/main/java/net/forsteri/createmorepotatoes/tileEntity/stationaryPotatoCanon/StationaryPotatoCanonInstance.java
+++ src/main/java/net/forsteri/createmorepotatoes/tileEntity/stationaryPotatoCanon/StationaryPotatoCanonInstance.java
@@ -8,40 +8,8 @@ import com.simibubi.create.content.contraptions.base.flwdata.RotatingData;
 import com.simibubi.create.content.contraptions.relays.encased.ShaftInstance;
 import net.minecraft.world.level.block.state.BlockState;
 
-public class StationaryPotatoCanonInstance extends KineticTileInstance<StationaryPotatoCanonTileEntity> {
-
+public class StationaryPotatoCanonInstance extends ShaftInstance {
     public StationaryPotatoCanonInstance(MaterialManager dispatcher, StationaryPotatoCanonTileEntity tile) {
         super(dispatcher, tile);
     }
-
-    protected RotatingData rotatingModel;
-
-    @Override
-    public void init() {
-        rotatingModel = setup(getModel().createInstance());
-    }
-
-    @Override
-    public void update() {
-        updateRotation(rotatingModel);
-    }
-
-    @Override
-    public void updateLight() {
-        relight(pos, rotatingModel);
-    }
-
-    @Override
-    public void remove() {
-        rotatingModel.delete();
-    }
-
-    protected BlockState getRenderedBlockState() {
-        return shaft();
-    }
-
-    protected Instancer<RotatingData> getModel() {
-        return getRotatingMaterial().getModel(getRenderedBlockState());
-    }
-
 }
