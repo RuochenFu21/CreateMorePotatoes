3f68994a5873d96c180248e69fa5c8ef6a8c2699
A_pythoner <62882800+Ruochenfu2011@users.noreply.github.com>
2022-10-18 08:26:19 +0800

    fix: extends DirectionalAxisKineticBlock now

--- src/main/java/net/forsteri/createmorepotatoes/tileEntity/stationaryPotatoCanon/StationaryPotatoCanonBlock.java
+++ src/main/java/net/forsteri/createmorepotatoes/tileEntity/stationaryPotatoCanon/StationaryPotatoCanonBlock.java
@@ -1,8 +1,10 @@
 package net.forsteri.createmorepotatoes.tileEntity.stationaryPotatoCanon;
 
+import com.simibubi.create.content.contraptions.base.DirectionalAxisKineticBlock;
 import com.simibubi.create.content.contraptions.base.DirectionalKineticBlock;
 import com.simibubi.create.content.contraptions.base.KineticBlock;
 import com.simibubi.create.content.contraptions.base.RotatedPillarKineticBlock;
+import com.simibubi.create.content.contraptions.goggles.IHaveGoggleInformation;
 import com.simibubi.create.foundation.block.ITE;
 import net.forsteri.createmorepotatoes.CreateMorePotatoes;
 import net.forsteri.createmorepotatoes.entry.ModTileEntities;
@@ -14,22 +16,9 @@ import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 
-public class StationaryPotatoCanonBlock extends RotatedPillarKineticBlock implements ITE<StationaryPotatoCanonTileEntity> {
+public class StationaryPotatoCanonBlock extends DirectionalAxisKineticBlock implements ITE<StationaryPotatoCanonTileEntity> {
     public StationaryPotatoCanonBlock(Properties properties) {
         super(properties);
-    }
-
-    @Override
-    public Direction.Axis getRotationAxis(BlockState state) {
-        CreateMorePotatoes.LOGGER.info("axis of stationary:" + state.getValue(AXIS));
-        return state.getValue(AXIS);
-    }
-
-    @Override
-    public boolean hasShaftTowards(LevelReader world, BlockPos pos, BlockState state, Direction face) {
-        CreateMorePotatoes.LOGGER.info("axis of shaft testing:" + face.getAxis());
-        CreateMorePotatoes.LOGGER.info("It's " + (getRotationAxis(state) == face.getAxis()));
-        return face.getAxis() == getRotationAxis(state);
     }
 
     @Override
--- src/main/java/net/forsteri/createmorepotatoes/tileEntity/stationaryPotatoCanon/StationaryPotatoCanonTileEntity.java
+++ src/main/java/net/forsteri/createmorepotatoes/tileEntity/stationaryPotatoCanon/StationaryPotatoCanonTileEntity.java
@@ -3,10 +3,14 @@ package net.forsteri.createmorepotatoes.tileEntity.stationaryPotatoCanon;
 import com.simibubi.create.content.contraptions.base.KineticTileEntity;
 import com.simibubi.create.content.contraptions.goggles.IHaveGoggleInformation;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.fluids.capability.CapabilityFluidHandler;
 
-public class StationaryPotatoCanonTileEntity extends KineticTileEntity implements IHaveGoggleInformation {
+import java.util.List;
+
+public class StationaryPotatoCanonTileEntity extends KineticTileEntity{
     public StationaryPotatoCanonTileEntity(BlockEntityType<?> typeIn, BlockPos pos, BlockState state) {
         super(typeIn, pos, state);
     }
