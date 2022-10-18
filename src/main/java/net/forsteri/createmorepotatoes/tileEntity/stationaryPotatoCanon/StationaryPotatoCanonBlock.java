1b3fc50f545e0450ec0df441056c96e0d46a43d6
A_pythoner <62882800+Ruochenfu2011@users.noreply.github.com>
2022-10-18 15:16:25 +0800

    test: log "SHOOTING" when shoot get called

--- src/main/java/net/forsteri/createmorepotatoes/tileEntity/stationaryPotatoCanon/StationaryPotatoCanonBlock.java
+++ src/main/java/net/forsteri/createmorepotatoes/tileEntity/stationaryPotatoCanon/StationaryPotatoCanonBlock.java
@@ -10,13 +10,22 @@ import net.forsteri.createmorepotatoes.CreateMorePotatoes;
 import net.forsteri.createmorepotatoes.entry.ModTileEntities;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.RotatedPillarBlock;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import org.jetbrains.annotations.NotNull;
 
+import java.util.Random;
+
+@SuppressWarnings("deprecation")
 public class StationaryPotatoCanonBlock extends DirectionalAxisKineticBlock implements ITE<StationaryPotatoCanonTileEntity> {
+
+    protected int timeOut;
     public StationaryPotatoCanonBlock(Properties properties) {
         super(properties);
     }
@@ -29,5 +38,20 @@ public class StationaryPotatoCanonBlock extends DirectionalAxisKineticBlock impl
     @Override
     public BlockEntityType<? extends StationaryPotatoCanonTileEntity> getTileEntityType() {
         return ModTileEntities.STATIONARY_POTATO_CANON.get();
+    }
+
+    public void neighborChanged(@NotNull BlockState pState, @NotNull Level pLevel, @NotNull BlockPos pPos, @NotNull Block pBlock, @NotNull BlockPos pFromPos, boolean pIsMoving) {
+        if (pLevel.hasNeighborSignal(pPos) && this.timeOut == 0) {
+            this.shoot(pLevel, pPos);
+        }
+    }
+
+    public void shoot(Level pLevel, BlockPos pPos){
+        CreateMorePotatoes.LOGGER.info("SHOOTING");
+        this.timeOut = 10;
+    }
+
+    public void tick(@NotNull BlockState pState, @NotNull ServerLevel pLevel, @NotNull BlockPos pPos, @NotNull Random pRand){
+        timeOut--;
     }
 }
