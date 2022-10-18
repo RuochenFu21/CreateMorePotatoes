7b2c34005970ef8d6882d42e4a40f5eb0c65c720
A_pythoner <62882800+Ruochenfu2011@users.noreply.github.com>
2022-10-18 08:43:38 +0800

    test: try adding shaft into the StationaryPotatoCanonRenderer

--- src/main/java/net/forsteri/createmorepotatoes/tileEntity/stationaryPotatoCanon/StationaryPotatoCanonRenderer.java
+++ src/main/java/net/forsteri/createmorepotatoes/tileEntity/stationaryPotatoCanon/StationaryPotatoCanonRenderer.java
@@ -1,10 +1,17 @@
 package net.forsteri.createmorepotatoes.tileEntity.stationaryPotatoCanon;
 
+import com.simibubi.create.content.contraptions.base.KineticTileEntity;
 import com.simibubi.create.content.contraptions.base.KineticTileEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
+import net.minecraft.world.level.block.state.BlockState;
 
 public class StationaryPotatoCanonRenderer extends KineticTileEntityRenderer {
     public StationaryPotatoCanonRenderer(BlockEntityRendererProvider.Context context) {
         super(context);
+    }
+
+    @Override
+    protected BlockState getRenderedBlockState(KineticTileEntity te) {
+        return shaft(getRotationAxisOf(te));
     }
 }
