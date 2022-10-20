96784f3ef812ebee84d7245c1ce3ffbdcc749742
A_pythoner <62882800+Ruochenfu2011@users.noreply.github.com>
2022-10-20 18:48:41 +0700

    fix: changed ProgrammableStationaryPotatoCannonRenderer

--- src/main/java/net/forsteri/createmorepotatoes/entry/ModTileEntities.java
+++ src/main/java/net/forsteri/createmorepotatoes/entry/ModTileEntities.java
@@ -4,6 +4,7 @@ import com.simibubi.create.foundation.data.CreateRegistrate;
 import com.tterrag.registrate.util.entry.BlockEntityEntry;
 import net.forsteri.createmorepotatoes.CreateMorePotatoes;
 import net.forsteri.createmorepotatoes.tileEntity.programmableStationaryPotatoCannon.ProgrammableStationaryPotatoCannonInstance;
+import net.forsteri.createmorepotatoes.tileEntity.programmableStationaryPotatoCannon.ProgrammableStationaryPotatoCannonRenderer;
 import net.forsteri.createmorepotatoes.tileEntity.programmableStationaryPotatoCannon.ProgrammableStationaryPotatoCannonTileEntity;
 import net.forsteri.createmorepotatoes.tileEntity.stationaryPotatoCannon.StationaryPotatoCannonInstance;
 import net.forsteri.createmorepotatoes.tileEntity.stationaryPotatoCannon.StationaryPotatoCannonRenderer;
@@ -23,7 +24,7 @@ public class ModTileEntities {
             .tileEntity("programmable_stationary_potato_cannon", ProgrammableStationaryPotatoCannonTileEntity::new)
             .instance(() -> ProgrammableStationaryPotatoCannonInstance::new)
             .validBlocks(ModBlocks.PROGRAMMABLE_STATIONARY_POTATO_CANNON_BLOCK)
-            .renderer(() -> StationaryPotatoCannonRenderer::new)
+            .renderer(() -> ProgrammableStationaryPotatoCannonRenderer::new)
             .register();
 
     public static void register(){
