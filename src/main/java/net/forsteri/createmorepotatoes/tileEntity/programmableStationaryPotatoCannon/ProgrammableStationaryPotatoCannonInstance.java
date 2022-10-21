043a3026d064142bac04cd3d265ca852f2c2a44d
A_pythoner <62882800+Ruochenfu2011@users.noreply.github.com>
2022-10-21 17:17:39 +0700

    test: renderer test

--- src/main/java/net/forsteri/createmorepotatoes/tileEntity/programmableStationaryPotatoCannon/ProgrammableStationaryPotatoCannonInstance.java
+++ src/main/java/net/forsteri/createmorepotatoes/tileEntity/programmableStationaryPotatoCannon/ProgrammableStationaryPotatoCannonInstance.java
@@ -1,21 +1,36 @@
 package net.forsteri.createmorepotatoes.tileEntity.programmableStationaryPotatoCannon;
 
+import com.jozufozu.flywheel.api.InstanceData;
+import com.jozufozu.flywheel.api.Instancer;
 import com.jozufozu.flywheel.api.MaterialManager;
+import com.simibubi.create.AllBlockPartials;
 import com.simibubi.create.AllBlocks;
 import com.simibubi.create.content.contraptions.base.KineticTileEntity;
+import com.simibubi.create.content.contraptions.base.KineticTileInstance;
 import com.simibubi.create.content.contraptions.base.SingleRotatingInstance;
+import com.simibubi.create.content.contraptions.base.flwdata.RotatingData;
 import com.simibubi.create.content.contraptions.relays.elementary.ShaftBlock;
 import net.forsteri.createmorepotatoes.entry.ModBlocks;
 import net.minecraft.core.Direction;
+import net.minecraft.world.level.LightLayer;
 import net.minecraft.world.level.block.state.BlockState;
 
-public class ProgrammableStationaryPotatoCannonInstance extends SingleRotatingInstance {
-    public ProgrammableStationaryPotatoCannonInstance(MaterialManager modelManager, KineticTileEntity tile) {
-        super(modelManager, tile);
-    }
+public class ProgrammableStationaryPotatoCannonInstance extends KineticTileInstance<ProgrammableStationaryPotatoCannonTileEntity> {
 
+    protected RotatingData shaftInstance;
+    public ProgrammableStationaryPotatoCannonInstance(MaterialManager modelManager, ProgrammableStationaryPotatoCannonTileEntity tile) {
+        super(modelManager, tile);
+        Instancer<RotatingData> shaft = getRotatingMaterial().getModel(AllBlockPartials.SHAFT_HALF, blockState, Direction.DOWN);
+        shaftInstance = shaft.createInstance();
+        shaftInstance.setRotationAxis(Direction.Axis.Y)
+                .setRotationalSpeed(tile.getSpeed())
+                .setRotationOffset(getRotationOffset(axis)).setColor(tile)
+                .setPosition(getInstancePosition())
+                .setBlockLight(world.getBrightness(LightLayer.BLOCK, pos))
+                .setSkyLight(world.getBrightness(LightLayer.SKY, pos));
+    }
     @Override
-    protected BlockState getRenderedBlockState() {
-        return ModBlocks.STATIONARY_POTATO_CANNON.getDefaultState();
+    protected void remove() {
+        shaftInstance.delete();
     }
 }
--- src/main/java/net/forsteri/createmorepotatoes/tileEntity/programmableStationaryPotatoCannon/ProgrammableStationaryPotatoCannonRenderer.java
+++ src/main/java/net/forsteri/createmorepotatoes/tileEntity/programmableStationaryPotatoCannon/ProgrammableStationaryPotatoCannonRenderer.java
@@ -1,10 +1,18 @@
 package net.forsteri.createmorepotatoes.tileEntity.programmableStationaryPotatoCannon;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.simibubi.create.AllBlockPartials;
 import com.simibubi.create.content.contraptions.base.KineticTileEntity;
 import com.simibubi.create.content.contraptions.base.KineticTileEntityRenderer;
 import com.simibubi.create.content.contraptions.relays.elementary.ShaftBlock;
+import com.simibubi.create.foundation.render.CachedBufferer;
+import com.simibubi.create.foundation.render.SuperByteBuffer;
+import com.simibubi.create.foundation.utility.AnimationTickHolder;
 import net.forsteri.createmorepotatoes.entry.ModBlocks;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.block.state.BlockState;
 
@@ -15,7 +23,24 @@ public class ProgrammableStationaryPotatoCannonRenderer extends KineticTileEntit
     }
 
     @Override
-    protected BlockState getRenderedBlockState(KineticTileEntity te) {
-        return ModBlocks.STATIONARY_POTATO_CANNON.getDefaultState();
+    protected void renderSafe(KineticTileEntity te, float partialTicks, PoseStack ms, MultiBufferSource buffer, int light, int overlay) {
+        SuperByteBuffer shaft = CachedBufferer.partialFacing(AllBlockPartials.SHAFT_HALF, te.getBlockState(), Direction.DOWN);
+        float offset = getRotationOffsetForPosition(te, te.getBlockPos(), Direction.Axis.Y);
+        float angle = (AnimationTickHolder.getRenderTime(te.getLevel()) * te.getSpeed() * 3f / 10) % 360;
+
+        if (te.getSpeed() != 0 && te.hasSource()) {
+            assert te.source != null;
+            BlockPos source = te.source.subtract(te.getBlockPos());
+            Direction sourceFacing = Direction.getNearest(source.getX(), source.getY(), source.getZ());
+            if (sourceFacing.getAxis() == Direction.DOWN.getAxis())
+                angle *= sourceFacing == Direction.DOWN ? 1 : -1;
+            else if (sourceFacing.getAxisDirection() == Direction.DOWN.getAxisDirection())
+                angle *= -1;
+        }
+
+        angle += offset;
+        angle = angle / 180f * (float) Math.PI;
+        kineticRotationTransform(shaft, te, Direction.Axis.Y, angle, light);
+        shaft.renderInto(ms, buffer.getBuffer(RenderType.solid()));
     }
 }
