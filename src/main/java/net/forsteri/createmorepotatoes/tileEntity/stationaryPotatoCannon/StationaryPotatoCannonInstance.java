a46292323299c72155f531ac47593db4284868b1
A_pythoner <62882800+Ruochenfu2011@users.noreply.github.com>
2022-10-21 17:07:05 +0700

    revert: wrong file

--- src/main/java/net/forsteri/createmorepotatoes/tileEntity/stationaryPotatoCannon/StationaryPotatoCannonInstance.java
+++ src/main/java/net/forsteri/createmorepotatoes/tileEntity/stationaryPotatoCannon/StationaryPotatoCannonInstance.java
@@ -1,28 +1,10 @@
 package net.forsteri.createmorepotatoes.tileEntity.stationaryPotatoCannon;
 
-import com.jozufozu.flywheel.api.Instancer;
 import com.jozufozu.flywheel.api.MaterialManager;
-import com.simibubi.create.AllBlockPartials;
-import com.simibubi.create.content.contraptions.base.KineticTileInstance;
-import com.simibubi.create.content.contraptions.base.flwdata.RotatingData;
-import net.minecraft.core.Direction;
-import net.minecraft.world.level.LightLayer;
+import com.simibubi.create.content.contraptions.relays.encased.ShaftInstance;
 
-public class StationaryPotatoCannonInstance extends KineticTileInstance<StationaryPotatoCannonTileEntity> {
+public class StationaryPotatoCannonInstance extends ShaftInstance {
     public StationaryPotatoCannonInstance(MaterialManager dispatcher, StationaryPotatoCannonTileEntity tile) {
         super(dispatcher, tile);
-        Instancer<RotatingData> shaft = getRotatingMaterial().getModel(AllBlockPartials.SHAFT_HALF, blockState, Direction.UP);
-        shaft.createInstance()
-                .setRotationAxis(Direction.Axis.Y)
-                .setRotationalSpeed(blockEntity.getSpeed())
-                .setRotationOffset(getRotationOffset(axis)).setColor(tile)
-                .setPosition(getInstancePosition())
-                .setBlockLight(world.getBrightness(LightLayer.BLOCK, pos))
-                .setSkyLight(world.getBrightness(LightLayer.SKY, pos));
-    }
-
-    @Override
-    protected void remove() {
-
     }
 }
--- src/main/java/net/forsteri/createmorepotatoes/tileEntity/stationaryPotatoCannon/StationaryPotatoCannonRenderer.java
+++ src/main/java/net/forsteri/createmorepotatoes/tileEntity/stationaryPotatoCannon/StationaryPotatoCannonRenderer.java
@@ -1,17 +1,8 @@
 package net.forsteri.createmorepotatoes.tileEntity.stationaryPotatoCannon;
 
-import com.mojang.blaze3d.vertex.PoseStack;
-import com.simibubi.create.AllBlockPartials;
 import com.simibubi.create.content.contraptions.base.KineticTileEntity;
 import com.simibubi.create.content.contraptions.base.KineticTileEntityRenderer;
-import com.simibubi.create.foundation.render.CachedBufferer;
-import com.simibubi.create.foundation.render.SuperByteBuffer;
-import com.simibubi.create.foundation.utility.AnimationTickHolder;
-import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
 import net.minecraft.world.level.block.state.BlockState;
 
 public class StationaryPotatoCannonRenderer extends KineticTileEntityRenderer {
@@ -20,25 +11,10 @@ public class StationaryPotatoCannonRenderer extends KineticTileEntityRenderer {
     }
 
     @Override
-    protected void renderSafe(KineticTileEntity te, float partialTicks, PoseStack ms, MultiBufferSource buffer, int light, int overlay) {
-        SuperByteBuffer shaft = CachedBufferer.partialFacing(AllBlockPartials.SHAFT_HALF, te.getBlockState(), Direction.DOWN);
-        float offset = getRotationOffsetForPosition(te, te.getBlockPos(), Direction.Axis.Y);
-        float angle = (AnimationTickHolder.getRenderTime(te.getLevel()) * te.getSpeed() * 3f / 10) % 360;
-
-        if (te.getSpeed() != 0 && te.hasSource()) {
-            BlockPos source = te.source.subtract(te.getBlockPos());
-            Direction sourceFacing = Direction.getNearest(source.getX(), source.getY(), source.getZ());
-            if (sourceFacing.getAxis() == Direction.DOWN.getAxis())
-                angle *= sourceFacing == Direction.DOWN ? 1 : -1;
-            else if (sourceFacing.getAxisDirection() == Direction.DOWN.getAxisDirection())
-                angle *= -1;
-        }
-
-        angle += offset;
-        angle = angle / 180f * (float) Math.PI;
-        kineticRotationTransform(shaft, te, Direction.Axis.Y, angle, light);
-        shaft.renderInto(ms, buffer.getBuffer(RenderType.solid()));
+    protected BlockState getRenderedBlockState(KineticTileEntity te) {
+        return shaft(getRotationAxisOf(te));
     }
+
 
     /*protected void renderSafe(StationaryPotatoCanonTileEntity te, float partialTicks, PoseStack ms, MultiBufferSource bufferSource, int light, int overlay) {
         *//* renderItem(te, partialTicks, ms, bufferSource, light, overlay); *//*
