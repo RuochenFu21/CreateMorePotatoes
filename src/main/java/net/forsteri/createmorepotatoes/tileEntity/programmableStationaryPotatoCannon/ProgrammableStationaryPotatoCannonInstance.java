23deae9d57c75ef68a0cfac195420e80553e423c
A_pythoner <62882800+Ruochenfu2011@users.noreply.github.com>
2022-10-23 19:26:20 +0800

    test: debug in beginFrame()

--- src/main/java/net/forsteri/createmorepotatoes/tileEntity/programmableStationaryPotatoCannon/ProgrammableStationaryPotatoCannonInstance.java
+++ src/main/java/net/forsteri/createmorepotatoes/tileEntity/programmableStationaryPotatoCannon/ProgrammableStationaryPotatoCannonInstance.java
@@ -59,5 +59,6 @@ public class ProgrammableStationaryPotatoCannonInstance extends KineticTileInsta
 
     @Override
     public void beginFrame() {
+        CreateMorePotatoes.LOGGER.info("Instancing");
     }
 }
