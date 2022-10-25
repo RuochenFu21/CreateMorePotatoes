33665b567d1c5740c7946de412ebf454448238f3
A_pythoner <62882800+Ruochenfu2011@users.noreply.github.com>
2022-10-25 13:13:24 +0800

    fix: using ANOTHER method to get theta

--- src/main/java/net/forsteri/createmorepotatoes/tileEntity/programmableStationaryPotatoCannon/ProgrammableStationaryPotatoCannonInstance.java
+++ src/main/java/net/forsteri/createmorepotatoes/tileEntity/programmableStationaryPotatoCannon/ProgrammableStationaryPotatoCannonInstance.java
@@ -1,23 +1,14 @@
 package net.forsteri.createmorepotatoes.tileEntity.programmableStationaryPotatoCannon;
 
-import com.jozufozu.flywheel.api.InstanceData;
 import com.jozufozu.flywheel.api.Instancer;
 import com.jozufozu.flywheel.api.Material;
 import com.jozufozu.flywheel.api.MaterialManager;
 import com.jozufozu.flywheel.api.instance.DynamicInstance;
 import com.jozufozu.flywheel.core.Materials;
 import com.jozufozu.flywheel.core.materials.model.ModelData;
-import com.simibubi.create.AllBlockPartials;
-import com.simibubi.create.AllBlocks;
-import com.simibubi.create.content.contraptions.base.KineticTileEntity;
-import com.simibubi.create.content.contraptions.base.KineticTileInstance;
 import com.simibubi.create.content.contraptions.base.SingleRotatingInstance;
-import com.simibubi.create.content.contraptions.base.flwdata.RotatingData;
-import com.simibubi.create.content.contraptions.relays.elementary.ShaftBlock;
 import net.forsteri.createmorepotatoes.CreateMorePotatoes;
-import net.forsteri.createmorepotatoes.entry.ModBlockPartial;
 import net.forsteri.createmorepotatoes.entry.ModBlocks;
-import net.minecraft.client.Minecraft;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.LightLayer;
 import net.minecraft.world.level.block.state.BlockState;
@@ -27,9 +18,12 @@ import java.util.Objects;
 public class ProgrammableStationaryPotatoCannonInstance extends SingleRotatingInstance implements DynamicInstance {
     protected ModelData cannonInstance;
 
+    protected ProgrammableStationaryPotatoCannonTileEntity tileEntity;
+
     protected double theta_now = 0;
     public ProgrammableStationaryPotatoCannonInstance(MaterialManager modelManager, ProgrammableStationaryPotatoCannonTileEntity tile) {
         super(modelManager, tile);
+        this.tileEntity = tile;
 
         Material<ModelData> mat = materialManager.defaultSolid()
                 .material(Materials.TRANSFORMED);
@@ -55,7 +49,7 @@ public class ProgrammableStationaryPotatoCannonInstance extends SingleRotatingIn
 
     @Override
     public void beginFrame() {
-        double theta = ((ProgrammableStationaryPotatoCannonTileEntity) Objects.requireNonNull(world.getExistingBlockEntity(getWorldPosition()))).theta;
+        double theta = tileEntity.theta;
         cannonInstance.rotateCentered(Direction.UP, (float) ((float) theta-theta_now));
         CreateMorePotatoes.LOGGER.info("framing! theta now:" + theta_now + ". theta we got: " + theta);
 //        cannonInstance.rotateCentered(Direction.UP, (float) ((ProgrammableStationaryPotatoCannonTileEntity) blockEntity).theta);
