cfd7f72992838bea63f8b1c295899b4c04a2ca9d
A_pythoner <62882800+Ruochenfu2011@users.noreply.github.com>
2022-10-25 11:37:11 +0800

    test: rotate 1 degrees now

--- src/main/java/net/forsteri/createmorepotatoes/tileEntity/programmableStationaryPotatoCannon/ProgrammableStationaryPotatoCannonInstance.java
+++ src/main/java/net/forsteri/createmorepotatoes/tileEntity/programmableStationaryPotatoCannon/ProgrammableStationaryPotatoCannonInstance.java
@@ -50,7 +50,7 @@ public class ProgrammableStationaryPotatoCannonInstance extends SingleRotatingIn
 
     @Override
     public void beginFrame() {
-        cannonInstance.rotate(45, Direction.Axis.Y);
+        cannonInstance.rotate(1, Direction.Axis.Y);
     }
     @Override
     protected BlockState getRenderedBlockState() {
